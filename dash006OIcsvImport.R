0f6675a98a9dc971d016921958eafca2033fe21d
Kate W <magpiedin@gmail.com>
2018-02-21 17:21:59 -0600

    updated OI import/merge/prep

--- dash006OIcsvImport.R
+++ dash006OIcsvImport.R
@@ -1,14 +1,12 @@
 ## collections-dashboard-prep
 #  Prep Oriental Institute catalogue data
 #
-# 1) Retrieve latest Penn Museum CSV dataset for "All" collections from:
-#       https://www.penn.museum/collections/objects/data.php
-#
+# 1) Retrieve latest OI CSV dataset...
 # 2) Move zipped CSV to "/data01raw" folder within this project directory.
-# 3) Unzip to a folder labelled "PM[YYYYMMDD]" -- e.g., "PM20180121"
-#           NOTE - Check that Penn's CSV-naming convention matches "all-YYYYMMDD.csv"
+# 3) Unzip to a folder labelled "OI[YYYYMMDD]" -- e.g., "OI20180121"
+#           NOTE - Check that OI dataset structure is correct
 #
-# 3) Run this script  
+# 4) Run this script  
 #       - NOTE: May need to re-set working directory to folder containing PM csv's
 #         (see lines 23 & 24)
 
@@ -18,8 +16,78 @@ print(paste(date(), "-- Starting OI Museum data import -- dash006OIcsvImport.R")
 # point to the directory containg the set of "Group" csv's from EMu
 setwd(paste0(origdir,"/data01raw/OI20180206"))
 
-OIList = list.files(pattern="OI.*.csv$")
-CatOI01 <- do.call(rbind, lapply(OIList, read.csv, stringsAsFactors = F))
+
+# make list of files into list of dataframes
+OIList = list.files(pattern="*.csv$")
+
+# # if need to check encoding:
+# #  ( thanks to: https://stackoverflow.com/a/4814870/4963125 )
+# codepages <- setNames(iconvlist(), iconvlist())
+# 
+# x <- lapply(codepages, function(enc) try(read.table(OIList[1],
+#                                                     fileEncoding=enc,
+#                                                     nrows=3, header=TRUE, sep=",")))
+# 
+# enc2check <- unique(do.call(rbind, sapply(x, dim)))
+# # check values in enc2check; the rowname with both values > 1 should be the encoding you need to use.
+# # CP1200 seems to work; alternatively, try UCS-2LE
+# # ...otherwise, if UTF-16: https://stat.ethz.ch/R-manual/R-devel/library/base/html/iconv.html
+
+# import files with assigned encoding
+list2env(
+  lapply(setNames(OIList, make.names(gsub(".csv$", "_OI", OIList))), # pretty names break stuff
+         read.table, 
+         fileEncoding="CP1200", 
+         header=T, sep=",",
+         stringsAsFactors = F),
+  envir = .GlobalEnv)
+
+# cleanup empty tables
+to.rm <- unlist(eapply(.GlobalEnv, function(x) is.data.frame(x) && NROW(x) < 1))
+rm(list = names(to.rm)[to.rm], envir = .GlobalEnv)
+
+# rename catalog 
+OIecatalog <- ecatalog_OI
+rm(ecatalog_OI)
+
+# prep multimedia tables 
+#   Need logic for PDFs?
+#   Need to re-export DetResourceType?
+MulMulti_OI$mediaURL <- paste0("https://oi-idb-static.uchicago.edu/multimedia/",
+                               MulMulti_OI$irn, "/",
+                               gsub("*.JPG",".1920x1200.jpg",
+                                    MulMulti_OI$MulIdentifier, ignore.case = T))
+
+# For now! (logic for PDFs & videos/DetResourceType should make this just a |-delim list of URLs)
+MulMulti_OI <- unite(MulMulti_OI, Media, irn:mediaURL, sep=" | ")
+
+
+# make a list of the OI dataframes 
+#  NOTE - might instead want import CSVs directly into list of dataframes?
+OIlist2 <- Filter(function(x) is(x, "data.frame"), mget(ls(pattern = "*_OI")))
+# do.call(rbind, as.list(ls(pattern = "*_OI")))
+
+
+# spread, unite & merge Multi-Value fields
+
+OImerged <- OIecatalog
+
+for (i in seq_along(OIlist2)) {
+  oiTable <- OIlist2[[i]][,-1]  # drop the local key field
+  oiNames <- colnames(oiTable)
+  colnames(oiTable)[2] <- "SpreadVal"
+  oiTable <- unique(oiTable)
+  oiTable$seq <- sequence(rle(as.character(oiTable[,1]))$lengths)
+  oiTableSpread <- spread(oiTable, seq, SpreadVal, fill = "")
+  oiTable2 <- unite(oiTableSpread, TabName, -ecatalogue_key, sep = " | ")
+  oiTable2$TabName <- gsub("(\\s+\\|)+", " |", oiTable2$TabName)
+  oiTable2$TabName <- gsub("(\\s+\\|\\s+)$", "", oiTable2$TabName)
+  colnames(oiTable2)[2] <- oiNames[2]
+  OImerged <- merge(OImerged, oiTable2, by = "ecatalogue_key", all.x=T)
+}
+
+
+
 
 setwd(paste0(origdir,"/data01raw/emuCat"))
 
