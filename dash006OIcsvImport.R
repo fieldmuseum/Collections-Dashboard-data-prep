76854e063dd8661ad9878ea61326481ff71f7e0e
Kate W <magpiedin@gmail.com>
2018-02-24 14:19:36 -0600

    added OI LUT prep

--- dash006OIcsvImport.R
+++ dash006OIcsvImport.R
@@ -18,13 +18,13 @@ setwd(paste0(origdir,"/data01raw/OI20180206"))
 
 
 # make list of files into list of dataframes
-OIList = list.files(pattern="*.csv$")
+OIlist = list.files(pattern="*.csv$")
 
 # # if need to check encoding:
 # #  ( thanks to: https://stackoverflow.com/a/4814870/4963125 )
 # codepages <- setNames(iconvlist(), iconvlist())
 # 
-# x <- lapply(codepages, function(enc) try(read.table(OIList[1],
+# x <- lapply(codepages, function(enc) try(read.table(OIlist[1],
 #                                                     fileEncoding=enc,
 #                                                     nrows=3, header=TRUE, sep=",")))
 # 
@@ -35,7 +35,7 @@ OIList = list.files(pattern="*.csv$")
 
 # import files with assigned encoding
 list2env(
-  lapply(setNames(OIList, make.names(gsub(".csv$", "_OI", OIList))), # pretty names break stuff
+  lapply(setNames(OIlist, make.names(gsub(".csv$", "_OI", OIlist))), # pretty names break stuff
          read.table, 
          fileEncoding="CP1200", 
          header=T, sep=",",
@@ -62,6 +62,66 @@ MulMulti_OI$mediaURL <- paste0("https://oi-idb-static.uchicago.edu/multimedia/",
 MulMulti_OI <- unite(MulMulti_OI, Media, irn:mediaURL, sep=" | ")
 
 
+# LUTs Prep ####
+
+# WHAT
+# ColClassification
+CatOI_class <- unique(OIecatalog[order(OIecatalog$ColClassification),c("ColClassification")])
+
+# CatDescription  # should unlist/break up to make shorter LUT / better search
+CatOI_desc <- unique(OIecatalog[order(OIecatalog$CatDescription),c("CatDescription")])
+
+# ProMaterials
+CatOI_mate
+
+# CatCollection
+CatOI_coll
+
+# InsDialect
+CatOI_dial
+
+# InsScript
+CatOI_scri
+
+
+# WHEN
+# DatKingRuler # also in WHO-LUT
+CatOI_king 
+
+# DatDynasty
+CatOI_dyna 
+
+# DatPeriod
+CatOI_peri
+
+# DatDateMade
+CatOI_made 
+
+# AcqDateReceived
+CatOI_rece 
+
+
+# WHERE
+# ProCountry
+CatOI_coun 
+
+# ProRegion
+CatOI_regi 
+# ArcLocus?
+
+
+# WHO
+
+CatOI_king # also in WHEN-LUT
+
+# ProAlternateNames
+CatOI_names <- unique(ProAlter_OI[order(ProAlter_OI$ProAlternateNames),c("ProAlternateNames")])
+
+# ProCulturalAffiliation
+CatOI_cult 
+
+
+
 # make a list of the OI dataframes 
 #  NOTE - might instead want import CSVs directly into list of dataframes?
 OIlist2 <- Filter(function(x) is(x, "data.frame"), mget(ls(pattern = "*_OI")))
@@ -86,104 +146,76 @@ for (i in seq_along(OIlist2)) {
   OImerged <- merge(OImerged, oiTable2, by = "ecatalogue_key", all.x=T)
 }
 
-
+rm(list = ls(pattern = "*_OI"))
+rm(OIlist, OIlist2, OIecatalog,
+   oiTable, oiTable2, oiTableSpread)
 
 
 setwd(paste0(origdir,"/data01raw/emuCat"))
 
-CatOI02 <- CatOI01[order(CatOI01$irn),-c(1:2)]
+CatOI02 <- OImerged[order(OImerged$irn),-1]
 CatOI02 <- unique(CatOI02)
-rm(CatOI01)
+rm(OImerged)
+
+# If need to drop columns &/or re-check uniqueness:
+# 
+# # build list of columns to drop
+# drops <- c("irn.1","DetResourceType","MulIdentifier",
+#            "ProAlternateNames", "ColClassification", "CatDescription")
+# 
+# CatOIunique <- unique(CatOI02[,!(names(CatOI02) %in% drops)])
+# 
+# # Remove duplicate irn's
+# # NOTE - for Egypt 2018-feb dataset, should have 59301 unique obj records
+# CatIRNcount <- NROW(levels(as.factor(CatOI02$irn)))
 
 
-# split out each of the Multivalue fields 
-  # - Multimedia
-  CatOI_MM <- unique(CatOI02[,c("irn", "irn.1", "DetResourceType", "MulIdentifier")])
-  CatOI_MM$IRNseq <- sequence(rle(as.character(CatOI_MM$irn))$lengths)
-  
-  # build MM URL
-  
-  # spread MM
-  CatOI_MMs <- spread(CatOI_MM, IRNseq, mmURL, fill="")
-  
-  # [repeat MM/multi-value spread for these 3 fields:]
-  # - ProAlternateNames
-  CatOI_names <- unique(CatOI02[,c("irn", "ProAlternateNames")])
-  
-  # - ColClassification
-  CatOI_class <- unique(CatOI02[,c("irn", "ColClassification")])
-  
-  # - CatDescription
-  CatOI_desc <- unique(CatOI02[,c("irn", "CatDescription")])
-
-  
-
-# build list of columns to drop
-drops <- c("irn.1","DetResourceType","MulIdentifier",
-           "ProAlternateNames", "ColClassification", "CatDescription")
-
-CatOIunique <- unique(CatOI02[,!(names(CatOI02) %in% drops)])
-
-# Remove duplicate irn's
-# NOTE - for Egypt 2018-feb dataset, should have 59301 unique obj records
-CatIRNcount <- NROW(levels(as.factor(CatOI02$irn)))
-
-
-
-
-
-#CatOI03 <- CatOI02[which(nchar(as.character(CatOI02$DarGlobalUniqueIdentifier)) > 3 & CatOI02$IRNseq == 1),]
-CatOI03 <- CatOI02[which(CatOI02$IRNseq == 1),]
-CatCheck <- CatOI02[which(CatOI02$IRNseq > 1),]
-
-CatOI03 <- dplyr::select(CatOI03, -IRNseq)
-
-CatOI04 <- data.frame(
-  "Group1_key" = CatOI03$irn,
+CatOI03 <- data.frame(
+  "Group1_key" = CatOI02$irn,
   "ecatalogue_key" = "",
-  "irn" = paste0("OI",CatOI03$irn),
-  "DarGlobalUniqueIdentifier" = CatOI03$url,
-  "AdmDateInserted" = "",
-  "AdmDateModified" = "",
-  "DarImageURL" = "",
-  "DarIndividualCount" = "",
+  "irn" = paste0("OI",CatOI02$irn),
+  "DarGlobalUniqueIdentifier" = CatOI02$AdmGUIDValue,
+  "AdmDateInserted" = CatOI02$AdmDateInserted,
+  "AdmDateModified" = CatOI02$AdmDateInserted,
+  "DarImageURL" = CatOI02$Media,
+  "DarIndividualCount" = "1",
   "DarBasisOfRecord" = "Artefact",
   "DarLatitude" = "",
   "DarLongitude" = "",
-  "DarCountry" = CatOI03$provenience,
-  "DarContinent" = CatOI03$curatorial_section,
-  "DarContinentOcean" = CatOI03$culture_area,
+  "DarCountry" = CatOI02$ProCountry,
+  "DarContinent" = "",
+  "DarContinentOcean" = "",
   "DarWaterBody" = "",
   "DarCollectionCode" = "Anthropology",
   "DarEarliestAge" = "",
   "DarEarliestEon" = "",
   "DarEarliestEpoch" = "",
   "DarEarliestEra" = "",
-  "DarEarliestPeriod" = CatOI03$date_made_early,
-  "AttPeriod_tab" = CatOI03$period,
-  "DesEthnicGroupSubgroup_tab" = CatOI03$culture,
-  "DesMaterials_tab" = CatOI03$material,
+  "DarEarliestPeriod" = CatOI02$DatDateMade,
+  "AttPeriod_tab" = CatOI02$DatPeriod,
+  "DesEthnicGroupSubgroup_tab" = CatOI02$ProC ,
+  "DesMaterials_tab" = CatOI02$material,
   "DarOrder" = "",
   "DarScientificName" = "",
   "ClaRank" = "",
   "ComName_tab" = "",
-  "DarRelatedInformation" = paste(CatOI03$native_name, 
-                                  CatOI03$description,
-                                  CatOI03$technique,
-                                  CatOI03$iconography,
+  "DarRelatedInformation" = paste(CatOI02$native_name, 
+                                  CatOI02$description,
+                                  CatOI02$technique,
+                                  CatOI02$iconography,
                                   sep = " | "),
-  "CatProject_tab" = paste(CatOI03$accession_credit_line,
-                           CatOI03$creator,
+  "CatProject_tab" = paste(CatOI02$accession_credit_line,
+                           CatOI02$creator,
                            sep = " | "),
   "DarYearCollected" = "",
   "DarMonthCollected" = "",
-  "EcbNameOfObject" = CatOI03$object_name,
+  "EcbNameOfObject" = CatOI02$object_name,
   "CatLegalStatus" = "",
   "CatDepartment" = "",
-  "DarCatalogNumber" = CatOI03$object_number,
+  "DarCatalogNumber" = CatOI02$object_number,
   "DarCollector" = "",
   "MulHasMultiMedia" = "",
-  "DarStateProvince" = CatOI03$provenience,
+  "DarStateProvince" = CatOI02$provenience,
   "DarInstitutionCode" = "OI",
   stringsAsFactors = F
 )
